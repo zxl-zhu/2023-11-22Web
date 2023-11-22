98b8a3bfbbbfad46e0bf2a206ae048aacbcc175a
zxl-zhu <65067429+zxl-zhu@users.noreply.github.com>
2023-11-23 00:28:23 +0900

    Update main.js

--- web/js/main.js
+++ web/js/main.js
@@ -2,30 +2,24 @@ jQuery(document).ready(function($) {
 
 	'use strict';
 
-        $(window).load(function() { // makes sure the whole site is loaded
-            $(".seq-preloader").fadeOut(); // will first fade out the loading animation
-            $(".sequence").delay(500).fadeOut("slow"); // will fade out the white DIV that covers the website.
+        $(window).load(function() { 
+            $(".seq-preloader").fadeOut(); 
+            $(".sequence").delay(500).fadeOut("slow"); 
         })
       
         
         $(function() {
   
         function showSlide(n) {
-            // n is relative position from current slide
-          
-            // unbind event listener to prevent retriggering
+
             $body.unbind("mousewheel");
           
-            // increment slide number by n and keep within boundaries
             currSlide = Math.min(Math.max(0, currSlide + n), $slide.length-1);
             
             var displacment = window.innerWidth*currSlide;
-            // translate slides div across to appropriate slide
             $slides.css('transform', 'translateX(-' + displacment + 'px)');
-            // delay before rebinding event to prevent retriggering
             setTimeout(bind, 700);
             
-            // change active class on link
             $('nav a.active').removeClass('active');
             $($('a')[currSlide]).addClass('active');
             
@@ -36,13 +30,11 @@ jQuery(document).ready(function($) {
           }
       
         function mouseEvent(e, delta) {
-            // On down scroll, show next slide otherwise show prev slide
             showSlide(delta >= 0 ? -1 : 1);
             e.preventDefault();
         }
         
         $('nav a, .main-btn a').click(function(e) {
-            // When link clicked, find slide it points to
             var newslide = parseInt($(this).attr('href')[1]);
             // find how far it is from current slide
             var diff = newslide - currSlide - 1;
@@ -51,21 +43,18 @@ jQuery(document).ready(function($) {
         });
       
         $(window).resize(function(){
-          // Keep current slide to left of window on resize
           var displacment = window.innerWidth*currSlide;
           $slides.css('transform', 'translateX(-'+displacment+'px)');
         });
         
-        // cache
         var $body = $('body');
         var currSlide = 0;
         var $slides = $('.slides');
         var $slide = $('.slide');
       
-        // give active class to first link
         $($('nav a')[0]).addClass('active');
         
-        // add event listener for mousescroll
+        // add event listener
         $body.bind('false', mouseEvent);
     })        
 
@@ -78,7 +67,6 @@ jQuery(document).ready(function($) {
             if($(window).scrollTop() > 100) {
                 $(".header").addClass("active");
             } else {
-                //remove the background property so it comes transparent again (defined in your css)
                $(".header").removeClass("active");
             }
         });
